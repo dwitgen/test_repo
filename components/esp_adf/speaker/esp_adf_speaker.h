bcb06210a0bd307dda649b9f1c8adacf484f3406
dwit <141568444+dwitgen@users.noreply.github.com>
2024-06-15 17:42:13 -0400

    Update esp_adf_speaker.h add play_url

--- components/esp_adf/speaker/esp_adf_speaker.h
+++ components/esp_adf/speaker/esp_adf_speaker.h
@@ -44,6 +44,7 @@ class ESPADFSpeaker : public ESPADFPipeline, public speaker::Speaker, public Com
 
   // Declare methods for media/http streaming
   void handle_mode_button();
+  void play_url();
 
   protected:
   void start_();
