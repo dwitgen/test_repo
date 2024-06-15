e391e8f061d75abd96d79d3200204a2b2c07aac3
dwit <141568444+dwitgen@users.noreply.github.com>
2024-06-15 10:50:52 -0400

    Update esp_adf_media_player.h change to use mediaplayertraits

--- components/esp_adf/media_player/esp_adf_media_player.h
+++ components/esp_adf/media_player/esp_adf_media_player.h
@@ -2,24 +2,20 @@
 #define ESP_ADF_MEDIA_PLAYER_H
 
 #include "esphome.h"
-#include "esp_adf_speaker.h"
 
 namespace esphome {
 namespace esp_adf_media_player {
 
-class ESPADFMediaPlayer : public Component, public MediaPlayer {
+class ESPADFMediaPlayer : public Component, public media_player::MediaPlayer {
  public:
   void setup() override;
   void loop() override;
-
-  MediaPlayerTraits get_traits() override;
-  void control(const MediaPlayerCall &call) override;
-
- private:
-  esphome::esp_adf_speaker::ESPADFSpeaker *speaker_;
+  media_player::MediaPlayerTraits get_traits() override;
+  void control(const media_player::MediaPlayerCall &call) override;
 };
 
 }  // namespace esp_adf_media_player
 }  // namespace esphome
 
 #endif  // ESP_ADF_MEDIA_PLAYER_H
+
