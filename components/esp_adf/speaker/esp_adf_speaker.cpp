cb5dcbb97a386b93e32120bbeb97c71ef5863e2c
dwit <141568444+dwitgen@users.noreply.github.com>
2024-06-15 22:12:59 -0400

    Update esp_adf_speaker.cpp

--- components/esp_adf/speaker/esp_adf_speaker.cpp
+++ components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -233,12 +233,26 @@ void ESPADFSpeaker::play_url(const std::string &url) {
         return;
     }
 
-    // Initialize Resample Filter
-    rsp_filter_cfg_t rsp_cfg = DEFAULT_RESAMPLE_FILTER_CONFIG();
-    rsp_cfg.src_rate = 44100;
-    rsp_cfg.src_ch = 2;
-    rsp_cfg.dest_rate = 44100;
-    rsp_cfg.dest_ch = 1; // Downmix to mono
+    rsp_filter_cfg_t rsp_cfg = {
+            .src_rate = 44100,
+            .src_ch = 2,
+            .dest_rate = 16000,
+            .dest_bits = 16,
+            .dest_ch = 1,
+            .src_bits = 16,
+            .mode = RESAMPLE_DECODE_MODE,
+            .max_indata_bytes = RSP_FILTER_BUFFER_BYTE,
+            .out_len_bytes = RSP_FILTER_BUFFER_BYTE,
+            .type = ESP_RESAMPLE_TYPE_AUTO,
+            .complexity = 2,
+            .down_ch_idx = 0,
+            .prefer_flag = ESP_RSP_PREFER_TYPE_SPEED,
+            .out_rb_size = RSP_FILTER_RINGBUFFER_SIZE,
+            .task_stack = RSP_FILTER_TASK_STACK,
+            .task_core = RSP_FILTER_TASK_CORE,
+            .task_prio = RSP_FILTER_TASK_PRIO,
+            .stack_in_ext = true,
+        };
     audio_element_handle_t filter = rsp_filter_init(&rsp_cfg);
     if (filter == NULL) {
         ESP_LOGE(TAG, "Failed to initialize resample filter");
