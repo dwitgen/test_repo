42b5912bc8cf6f831c9337ef76a2a69a3970d0b9
dwit <141568444+dwitgen@users.noreply.github.com>
2024-06-19 09:16:01 -0400

    Update esp_adf_speaker.h add button handler

--- components/esp_adf/speaker/esp_adf_speaker.h
+++ components/esp_adf/speaker/esp_adf_speaker.h
@@ -33,6 +33,7 @@ class ESPADFSpeaker : public ESPADFPipeline, public speaker::Speaker, public Com
   bool has_buffered_data() const override;
 
   // Declare methods for volume control
+  void button_event_handler(void *handler_args, esp_event_base_t base, int32_t id, void *event_data);
   void set_volume(int volume);
   void volume_up();
   void volume_down();
