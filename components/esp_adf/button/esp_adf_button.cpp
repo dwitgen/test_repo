364572707b9e061f957cf5824f2963b47c6d9923
dwit <141568444+dwitgen@users.noreply.github.com>
2024-06-21 08:19:00 -0400

    Update esp_adf_button.cpp

--- components/esp_adf/button/esp_adf_button.cpp
+++ components/esp_adf/button/esp_adf_button.cpp
@@ -32,11 +32,11 @@ void handle_button_event(int32_t id) {
         switch (id) {
             case BUTTON_VOLUP_ID:
                 ESP_LOGI(TAG, "Volume up detected");
-                volume_up();
+                //volume_up();
                 break;
             case BUTTON_VOLDOWN_ID:
                 ESP_LOGI(TAG, "Volume down detected");
-                volume_down();
+               // volume_down();
                 break;
             case BUTTON_SET_ID:
                 ESP_LOGI(TAG, "Set button detected");
