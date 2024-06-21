6bc145eed677dd2c7f316f4887cd6d640890dc8b
dwit <141568444+dwitgen@users.noreply.github.com>
2024-06-21 09:56:44 -0400

    Update esp_adf_speaker.cpp

--- components/esp_adf/speaker/esp_adf_speaker.cpp
+++ components/esp_adf/speaker/esp_adf_speaker.cpp
@@ -219,7 +219,7 @@ void ESPADFSpeaker::setup() {
     };
     periph_service_handle_t input_ser = input_key_service_create(&input_cfg);
     input_key_service_add_key(input_ser, input_key_info, INPUT_KEY_NUM);
-    periph_service_set_callback(input_ser, ESPADFSpeaker::input_key_service_cb, this); */
+    periph_service_set_callback(input_ser, ESPADFSpeaker::input_key_service_cb, this); 
 
     this->initialize_audio_pipeline();
 }
