b2b99ae4feec6dbce90c7d9e18c1d400373258ab
dwit <141568444+dwitgen@users.noreply.github.com>
2024-07-14 21:37:30 -0400

    Update esp_adf_button.h

--- components/esp_adf/button/esp_adf_button.h
+++ components/esp_adf/button/esp_adf_button.h
@@ -15,10 +15,10 @@ class ButtonHandler {
  public:
   static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx);
   static void handle_button_event(ESPADFSpeaker *instance, int32_t id, int32_t event_type);
-  static void handle_mode_button();
-  static void handle_play_button();
-  static void handle_set_button();
-  static void handle_rec_button();
+  static void handle_mode_button(ESPADFSpeaker *instance);
+  static void handle_play_button(ESPADFSpeaker *instance);
+  static void handle_set_button(ESPADFSpeaker *instance);
+  static void handle_rec_button(ESPADFSpeaker *instance);
   static void volume_up(ESPADFSpeaker *instance);
   static void volume_down(ESPADFSpeaker *instance);
 };
