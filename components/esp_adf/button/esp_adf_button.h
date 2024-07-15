e778e9d69bef2cc130881ba88c0160b2eaed9aa6
dwit <141568444+dwitgen@users.noreply.github.com>
2024-07-14 21:04:06 -0400

    Update esp_adf_button.h

--- components/esp_adf/button/esp_adf_button.h
+++ components/esp_adf/button/esp_adf_button.h
@@ -10,10 +10,12 @@
 namespace esphome {
 namespace esp_adf {
 
+class ESPADFSpeaker;  // Forward declaration
+
 class ButtonHandler {
  public:
   static esp_err_t input_key_service_cb(periph_service_handle_t handle, periph_service_event_t *evt, void *ctx);
-  static void handle_button_event(int32_t id, int32_t event_type);
+  static void handle_button_event(ESPADFSpeaker *instance, int32_t id, int32_t event_type);
   static void handle_mode_button();
   static void handle_play_button();
   static void handle_set_button();
